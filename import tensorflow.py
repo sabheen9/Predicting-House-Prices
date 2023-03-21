5cb9b91ea4f50a02448777e0d103bc11942ec3ec
sabheen9 <sabheen_ammad@hotmail.com>
2023-03-21 01:58:41 -0700

    1st

---
+++ import tensorflow.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from utils import *
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, LambdaCallback
+
+%matplotlib inline
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+print('Libraries imported.')
